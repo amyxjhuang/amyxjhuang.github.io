007cfa371c9f49954f3689d0707fa59c40dbd9ab
Amy Huang <amyxjhuang@berkeley.edu>
2022-06-13 17:49:56 -0700

    fjdlksj

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ class App extends React.Component {
             {/* <img src="https://sdk.bitmoji.com/render/panel/965f4060-c8d0-4b02-8e05-45f7beb660eb-32f4148d-fa0e-4a7f-8810-f7e01716acf4-v1.png?transparent=1&palette=1" height="100px"/>amy x huang</h1> */}
             </a>
           <div id="nav">
-            <a className="nav-item" href="/">home</a>
+          <a className="nav-item" href="/">home</a>
+          <a className="nav-item" target="_" href="/art">sewing</a>
             <a className="nav-item" target="_" href="/weather"><img src={require("./images/cloud.png")} height="30px"/></a>
 
           </div>
