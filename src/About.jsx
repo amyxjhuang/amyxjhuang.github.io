9afdb3b93dabfc72cf6dfd55866fa98951840eac
Amy Huang <amyxjhuang@berkeley.edu>
2022-06-22 22:15:10 -0700

    about me thing

--- src/About.jsx
+++ src/About.jsx
@@ -8,13 +8,13 @@ class About extends React.Component {
   render() {
       return (
         <div className="container">
-        <h3>
+        
+          <div className="row">
+          <div className="container">
+          <h3>
           {/* <img src={require("./images/flower.png")} height="50px"/> */}
         about me</h3>
-          <div className="row">
-
-          <div className="container">
-          <div style={{color:"#685344"}}><i >software developer based in </i><img height="20px" src="https://static.thenounproject.com/png/1832-200.png" /> <i>manhattan nyc</i></div>
+          {/* <div style={{color:"#685344"}}><i >software developer based in </i><img height="20px" src="https://static.thenounproject.com/png/1832-200.png" /> <i>manhattan nyc</i></div> */}
 
               <p><br/>hello! :)  my name is Amy Huang, & i love all things arts n crafts n coding. i'm a recent grad at uc berkeley with a bachelor's in computer science! 
                 this year i will also be starting work at pinterest as a fullstack software developer based in nyc.  </p>
