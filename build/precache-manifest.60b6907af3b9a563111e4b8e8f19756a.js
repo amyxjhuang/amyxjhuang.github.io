d3222910bde657032af8b52ea13568fe035d65fa
amyxjhuang <amyxjhuang@berkeley.edu>
2023-10-31 19:10:23 -0400

    minor edits

---
+++ build/precache-manifest.60b6907af3b9a563111e4b8e8f19756a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1796fcc34cde8f0f69b20bcfc8bcd03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4aa7d5edef6d43c3b5b",
+    "url": "/static/css/main.b766570d.chunk.css"
+  },
+  {
+    "revision": "cd69bfcd8e6032c1d9d8",
+    "url": "/static/js/2.898682b8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.898682b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4aa7d5edef6d43c3b5b",
+    "url": "/static/js/main.0d1520db.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "0ffb7698b25d897cac1676b5f28dcaa5",
+    "url": "/static/media/mes.0ffb7698.png"
+  },
+  {
+    "revision": "4baa5cee2f50d61d5f9a5fdc121b6e96",
+    "url": "/static/media/straw1.4baa5cee.png"
+  },
+  {
+    "revision": "45c90b8d1bee8c5aef9b36df83554fc4",
+    "url": "/static/media/straw2.45c90b8d.png"
+  },
+  {
+    "revision": "36c1e3a53ea86e18a7f94cb1e1556be8",
+    "url": "/static/media/wheres-food.36c1e3a5.png"
+  }
+]);
