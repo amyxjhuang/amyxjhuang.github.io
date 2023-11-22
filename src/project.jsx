c6a53289108b6d181a6a784f9bfef444de545ac6
Amy Huang <amyxjhuang@berkeley.edu>
2023-11-21 19:37:03 -0500

    add project

--- src/project.jsx
+++ src/project.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import './App.css'
+import React from "react";
+import "./App.css";
 // import { Player } from 'video-react';
-
 
 class Project extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
-    var bulletList = this.props.bulletList.map((b) =>
-      <li key={b}>{b}</li>
-    );
-    var imgVid =  <img className="proj-img" src={this.props.imgsrc}/>
+    var bulletList = this.props.bulletList.map((b) => <li key={b}>{b}</li>);
+    var imgVid = <img className="proj-img" src={this.props.imgsrc} />;
 
-    return(
-
+    return (
       <div className="project-container">
+        <div className="proj-desc">
+          <a target="_blank" href={this.props.link}> {imgVid}</a>
+        </div>
+        <div className="proj-desc">
+          <div>
+            <h4>{this.props.title}</h4>
+            <p>{this.props.desc}</p>
+            <a target="_blank" href={this.props.link}> {this.props.urlDesc} </a>
 
-       <div className="proj-desc">
-
-            <a href={this.props.link}> {imgVid}</a></div>
-        <div className="proj-desc"> 
-        <div>
-          <h4>{this.props.title}</h4>
-          <p>{this.props.desc}</p>
-          <a href={this.props.link}> {this.props.urlDesc} </a>
-
-          <div className="proj-bullets"><ul>{bulletList}</ul></div>
+            <div className="proj-bullets">
+              <ul>{bulletList}</ul>
+            </div>
           </div>
         </div>
       </div>
@@ -35,6 +32,5 @@ class Project extends React.Component {
   }
 }
 // <img src={this.props.imgsrc} />
-
 
 export default Project;
